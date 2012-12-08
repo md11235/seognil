bea06da96ca3bc816b1a2812c4826ed8962b0ef0
Madsen Zhang <zhang.sen@laxino.com>
2012-12-08 09:23:18 +0800

    add test program.

---
+++ seognil-test.el
@@ -0,0 +1,20 @@
+;; make the content inside WORDS-LIST-FILENAME be like:
+;; ("word-1" "word-2" "word-3"), a list containing the words to be
+;; tested(queried) in the dictionary DICTIONARY-NAMEn
+(defun seognil-test-query-words-in-dict (words-list-filename dicitonary-name)
+  (with-temp-buffer
+    (insert-file-contents words-list-filename)
+    (let ((words-list (read (buffer-substring-no-properties (point-min)
+                                                            (point-max)))))
+      (loop for word in words-list
+            do (assert (let ((result (seognil-word-definition-position dicitonary-name word)))
+                         (and (not (null result))
+                              (string-equal word
+                                            (car result)))
+                         t))))))
+
+;; run the test using
+;; (seognil-test-query-words-in-dict "adict-words-list.txt" "ADict")
+
+
+
